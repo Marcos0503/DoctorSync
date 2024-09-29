93cdd0248185622b7ecc32db34a52456cd056a25
Marcos0503 <Marcoshenrique96126638@gmail.com>
2024-09-29 00:47:43 -0300

    Adicionando composer.json ou index.php

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'seu_usuario/hospital-sistema-chat',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '93f985b22113f537a1557c895c2d57506499807d',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'seu_usuario/hospital-sistema-chat' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '93f985b22113f537a1557c895c2d57506499807d',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
